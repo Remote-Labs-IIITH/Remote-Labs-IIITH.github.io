13a540a81195bc5ed6b4f1976664d4f68bae2c87
AkshitGureja <akgureja28@gmail.com>
2022-12-13 00:20:32 +0530

    almost done with RC

--- rc_circuit/script.js
+++ rc_circuit/script.js
@@ -1,36 +1,8 @@
-var myInterval;
-let UserVR2;
-
-function myfunction() {
-    // console.log("tu pagal hai kya??")
-    document.getElementById("bt1").innerHTML = "Recalibrating";
-    fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1" + "1")
-    myInterval = setInterval(myTimer, 500);
-}
-
-function myTimer() {
-    //document.querySelector('form').onsubmit = function () {
-    fetch('https://blr1.blynk.cloud/external/api/get?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1')
-        .then(response => response.json())
-        .then(data => {
-            const myJSON = JSON.stringify(data)
-            if (myJSON == "0") {
-                document.getElementById("bt1").innerHTML = "Recalibrate";
-                clearInterval(myInterval);
-                document.querySelector("#u").value = 0;
-                // document.querySelector("#v").value = 0;
-                document.querySelector("#us").innerHTML = "0";
-                // document.querySelector("#vs").innerHTML = "0";
-            }
-        });
-    // .catch(error => {
-    //     console.log('Error:', error);
-    // })
-    return false;
-}
-
 var saved_u_val;
-// var saved_v_val;
+
+inputslider_u.onmouseup = () => {
+    document.getElementById("field1_input").innerHTML = document.getElementById("u").value;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -43,48 +15,19 @@ document.addEventListener('DOMContentLoaded', () => {
         location.href = '../home_page/home.html'
     }
 
-    fetch("https://blr1.blynk.cloud/external/api/get?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v5")
-      .then(response => response.json())
-      .then(data => {
-         UserVR2 = Number(JSON.stringify(data)) + 1
-        //  document.getElementById('count').innerHTML = `User Count : ${UserVR2}`;
-   })
-
     saved_u_val = sessionStorage.getItem('saved_val_u');
-    // saved_v_val = sessionStorage.getItem('saved_val_v');
-    // console.log(saved_u_val);
-    // console.log(saved_v_val);
 
     if(saved_u_val == null){
         saved_u_val = 0;
-        // saved_v_val = 0;
         sessionStorage.setItem('saved_val_u', saved_u_val);
-        // sessionStorage.setItem('saved_val_v', saved_v_val);
-        // console.log(saved_u_val);
-        // console.log(saved_v_val);
     }
-    // console.log('Yes, I am mad');
-    // document.querySelector("#vs").innerHTML = saved_v_val;
     document.querySelector("#us").innerHTML = saved_u_val;
     document.querySelector("#u").value = saved_u_val;
-    // document.querySelector("#v").value = saved_v_val;
-
-    //setInterval(myTimer, 500);
-
-    // document.querySelector('form').onsubmit = function () {
-    //     const target = document.querySelector('#get_bool').value;
-    //     document.querySelector('#result2').innerHTML = target;
-    //     //console.log(target);
-    //     fetch("https://blynk.cloud/external/api/update?token=PIhHUS2rhKB6AlJQeN7TGcMh326zGV4-&v1=" + target)
-    //     // document.querySelector('#result').innerHTML = target;
-    //     return false;
-    // }
 
     const inputslider_u = document.querySelector("#u");
     inputslider_u.oninput = () => {
         let value1 = inputslider_u.value;
-        document.querySelector("#us").innerHTML = value1;
-        fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1=" + value1)
+        document.querySelector("#field1_input").innerHTML = value1;
         sessionStorage.setItem('saved_val_u', value1);
 
     return false;
@@ -92,45 +35,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-function Up(ID){
-    document.getElementById(ID).value = 0;
-    fetch("https://blr1.blynk.cloud/external/api/get?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1")
-    .then(response => response.json())
-    .then(data => {
-        const myJSON = JSON.stringify(data);
-        if(myJSON == 1){
-            fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1=0")
-        }
-    })
-}
-
-function Down(ID){
-    document.getElementById(ID).value = 1;
-
-    fetch("https://blr1.blynk.cloud/external/api/get?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1")
-    .then(response => response.json())
-    .then(data => {
-        const myJSON = JSON.stringify(data);
-        if(myJSON == 0){
-            fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1=1")
-        }
-    })
-}
-
 function increment(ID) {
     const cur = document.getElementById(ID);
     var v1 = cur.value;
     v1 = (Number(v1) + Number(cur.step));
-    if (Number(v1) > 9600) {
-        v1 = 9600;
+    if (Number(v1) > 9675) {
+        v1 = 9675;
     }
    
     cur.value = v1;
-    document.getElementById(ID + "s").innerHTML = v1;
+    document.getElementById("field1_input").innerHTML = v1;
     
     console.log(String.fromCharCode(ID.charCodeAt(0) - 68));
     sessionStorage.setItem('saved_val_u', v1);
-    fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1=" + v1)
 }
 
 function decrement(ID) {
@@ -141,30 +58,21 @@ function decrement(ID) {
         v1 = 0;
     }
     cur.value = v1;
-    document.getElementById(ID + "s").innerHTML = v1;
-    // console.log(String.fromCharCode(ID.charCodeAt(0) - 68));
+    document.getElementById("field1_input").innerHTML = v1;
     sessionStorage.setItem('saved_val_u', v1);
-    // if(curr_state != 1){
-    fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1=" + v1)
 }
 
 function leave(){
     sessionStorage.removeItem("saved_countdown");
-    sessionStorage.removeItem("saved_val_v");
     sessionStorage.removeItem("saved_val_u");
-    fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v5=" + UserVR2)
-    fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v3=0")
-    fetch("https://blr1.blynk.cloud/external/api/update?token=xhn8z8fVu6weEbbHIgVNjmQ4waWrX6Xm&v1=0")
-        .then(() => {
-            location.replace("../home_page/home.html")
-        })
+    location.replace("../home_page/home.html")
 }
 
 function return_home(){
     location.href='../home_page/home.html'
 }
 
-window.addEventListener('beforeunload', () => {
-    fetch("https://blr1.blynk.cloud/external/api/update?token=R77dMWPsQ8B7xavEV_HVjaVF01DklJji&v0=" + "1")
-    fetch('https://blr1.blynk.cloud/external/api/update?token=R77dMWPsQ8B7xavEV_HVjaVF01DklJji&v3=0')
-})
+// window.addEventListener('beforeunload', () => {
+//     fetch("https://blr1.blynk.cloud/external/api/update?token=R77dMWPsQ8B7xavEV_HVjaVF01DklJji&v0=" + "1")
+//     fetch('https://blr1.blynk.cloud/external/api/update?token=R77dMWPsQ8B7xavEV_HVjaVF01DklJji&v3=0')
+// })
